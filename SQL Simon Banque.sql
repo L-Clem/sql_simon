f06ae36726c3a022185ff8c72ccb699e131c6400
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-04-16 12:17:28 +0200

    Diagram v2.6 & Seeder v2.1

--- SQL Simon Banque.sql
+++ SQL Simon Banque.sql
@@ -1,105 +1,114 @@
-CREATE TABLE `address` (
-	`pk_address` INT PRIMARY KEY AUTO_INCREMENT,
-	`street_number` SMALLINT NOT NULL,
-	`street` VARCHAR(250) NOT NULL,
-	`address_complement` VARCHAR(100),
-	`fk_city` INT NOT NULL
+CREATE TABLE 'address' (
+	'pk_address' INT PRIMARY KEY AUTO_INCREMENT,
+	'street_number' SMALLINT NOT NULL,
+	'street' VARCHAR(250) NOT NULL,
+	'address_complement' VARCHAR(100),
+	'fk_city' INT NOT NULL
 );
 
-CREATE TABLE `city` (
-	`pk_city` INT PRIMARY KEY AUTO_INCREMENT,
-	`name` VARCHAR(50) UNIQUE NOT NULL,
-	`postal_code` VARCHAR(5) NOT NULL,
-	`fk_department` INT NOT NULL
+CREATE TABLE 'city' (
+	'pk_city' INT PRIMARY KEY AUTO_INCREMENT,
+	'name' VARCHAR(50) UNIQUE NOT NULL,
+	'postal_code' VARCHAR(5) NOT NULL,
+	'fk_department' INT NOT NULL
 );
 
-CREATE TABLE `region` (
-	`pk_region` INT PRIMARY KEY AUTO_INCREMENT,
-	`name` VARCHAR(25) UNIQUE NOT NULL
+CREATE TABLE 'region' (
+	'pk_region' INT PRIMARY KEY AUTO_INCREMENT,
+	'name' VARCHAR(25) UNIQUE NOT NULL,
+	'country' VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `department` (
-	`pk_departement` INT PRIMARY KEY AUTO_INCREMENT,
-	`code` CHAR(3) UNIQUE NOT NULL,
-	`name` VARCHAR(25) UNIQUE NOT NULL,
-	`fk_region` INT NOT NULL
+CREATE TABLE 'department' (
+	'pk_departement' INT PRIMARY KEY AUTO_INCREMENT,
+	'code' CHAR(3) UNIQUE NOT NULL,
+	'name' VARCHAR(25) UNIQUE NOT NULL,
+	'fk_region' INT NOT NULL
 );
 
-CREATE TABLE `client` (
-	`pk_client` INT PRIMARY KEY AUTO_INCREMENT,
-	`first_name` VARCHAR(1000) NOT NULL,
-	`last_name` VARCHAR(1000) NOT NULL,
-	`salary_per_month` decimal(8,2) NOT NULL,
-	`fk_advisor` INT NOT NULL
+CREATE TABLE 'client' (
+	'pk_client' INT PRIMARY KEY AUTO_INCREMENT,
+	'first_name' VARCHAR(1000) NOT NULL,
+	'last_name' VARCHAR(1000) NOT NULL,
+	'salary_per_month' decimal(8,2) NOT NULL,
+	'fk_advisor' INT NOT NULL,
+	'fk_address' INT NOT NULL
 );
 
-CREATE TABLE `client_account_type` (
-	`pk_client_account_type` INT PRIMARY KEY AUTO_INCREMENT,
-	`fk_client` INT,
-	`fk_account_type` INT
+CREATE TABLE 'client_account_type' (
+	'pk_client_account_type' INT PRIMARY KEY AUTO_INCREMENT,
+	'fk_client' INT NOT NULL,
+	'fk_account_type' INT NOT NULL,
+	'opening' DATETIME NOT NULL, 
+	'ending' DATETIME NOT NULL
 );
 
 
-CREATE TABLE `agency` (
-	`pk_agency` INT PRIMARY KEY AUTO_INCREMENT,
-	`name` VARCHAR(500) NOT NULL,
-	`phone_number` VARCHAR(13) NOT NULL,
-	`fw_address` INT NOT NULL
+CREATE TABLE 'agency' (
+	'pk_agency' INT PRIMARY KEY AUTO_INCREMENT,
+	'name' VARCHAR(500) NOT NULL,
+	'phone_number' VARCHAR(13) NOT NULL,
+	'fw_address' INT NOT NULL
 );
 
-CREATE TABLE `agency_advisor` (
-	`pk_agency_advisor` INT PRIMARY KEY AUTO_INCREMENT,
-	`fk_agency` INT,
-	`fk_advisor` INT
+CREATE TABLE 'agency_advisor' (
+	'pk_agency_advisor' INT PRIMARY KEY AUTO_INCREMENT,
+	'fk_agency' INT,
+	'fk_advisor' INT
 );
 
-CREATE TABLE `advisor` (
-	`pk_advisor` INT PRIMARY KEY AUTO_INCREMENT,
-	`first_name` VARCHAR(1000) NOT NULL,
-	`last_name` VARCHAR(1000) NOT NULL,
-	`status` ENUM ('working', 'vacation', 'gone') NOT NULL
+CREATE TABLE 'advisor' (
+	'pk_advisor' INT PRIMARY KEY AUTO_INCREMENT,
+	'first_name' VARCHAR(1000) NOT NULL,
+	'last_name' VARCHAR(1000) NOT NULL,
+	'status' ENUM ('working', 'vacation', 'gone') NOT NULL
 );
 
-CREATE TABLE `transaction` (
-	`pk_transaction` INT PRIMARY KEY AUTO_INCREMENT,
-	`amount` DECIMAL(8,2) NOT NULL,
-	`emission` DATETIME NOT NULL,
-	`fk_sender_client` INT NOT NULL,
-	`fk_receiver_client` INT NOT NULL
+CREATE TABLE 'transaction' (
+	'pk_transaction' INT PRIMARY KEY AUTO_INCREMENT,
+	'amount' DECIMAL(8,2) NOT NULL,
+	'emission' DATETIME NOT NULL,
+	'fk_sender_account_type' INT NOT NULL,
+	'fk_receiver_account_type' INT NOT NULL,
+	'fk_sender_client' INT NOT NULL,
+	'fk_receiver_client' INT NOT NULL
 );
 
-CREATE TABLE `account_type` (
-	`pk_account_type` INT PRIMARY KEY AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL,
-	`interest_rate` FLOAT(3,2) DEFAULT NULL,
-	`opening` DATETIME NOT NULL, 
-	`ending` DATETIME NOT NULL,
-	`risk` ENUM ('low', 'medium', 'high') NOT NULL,
-	`taxation` BOOLEAN
+CREATE TABLE 'account_type' (
+	'pk_account_type' INT PRIMARY KEY AUTO_INCREMENT,
+	'name' VARCHAR(255) NOT NULL,
+	'interest_rate' FLOAT(3,2) DEFAULT NULL,
+	'risk' ENUM ('low', 'medium', 'high') NOT NULL,
+	'taxation' BOOLEAN DEFAULT FALSE,
+	'is_external' BOOLEAN DEFAULT FALSE
 );
 
-/*CREATE INDEX `account_index_0` ON `account` (`iban`, `swift`);
+/*CREATE INDEX 'account_index_0' ON 'account' ('iban', 'swift');
 
-CREATE INDEX `advisor_index_1` ON `advisor` (`status`);
+CREATE INDEX 'advisor_index_1' ON 'advisor' ('status');
 
-CREATE INDEX `loan_index_2` ON `loan` (`opening`);*/
+CREATE INDEX 'loan_index_2' ON 'loan' ('opening');*/
 
-ALTER TABLE `address` ADD FOREIGN KEY (`fk_city`) REFERENCES `city` (`pk_city`);
+ALTER TABLE 'address' ADD FOREIGN KEY ('fk_city') REFERENCES 'city' ('pk_city');
 
-ALTER TABLE `city` ADD FOREIGN KEY (`fk_department`) REFERENCES `department` (`pk_departement`);
+ALTER TABLE 'city' ADD FOREIGN KEY ('fk_department') REFERENCES 'department' ('pk_departement');
 
-ALTER TABLE `department` ADD FOREIGN KEY (`fk_region`) REFERENCES `region` (`pk_region`);
+ALTER TABLE 'department' ADD FOREIGN KEY ('fk_region') REFERENCES 'region' ('pk_region');
 
-ALTER TABLE `client` ADD FOREIGN KEY (`fk_advisor`) REFERENCES `advisor` (`pk_advisor`);
+ALTER TABLE 'client' ADD FOREIGN KEY ('fk_advisor') REFERENCES 'advisor' ('pk_advisor');
+ALTER TABLE 'client' ADD FOREIGN KEY ('fk_address') REFERENCES 'address' ('pk_address');
 
-ALTER TABLE `client_account_type` ADD FOREIGN KEY (`fk_client`) REFERENCES `client`(`pk_client`);
-ALTER TABLE `client_account_type` ADD FOREIGN KEY (`fk_account_type`) REFERENCES `account_type`(`pk_account_type`);
 
-ALTER TABLE `transaction` ADD FOREIGN KEY (`fk_sender_client`) REFERENCES `account_type` (`pk_account_type`);
-ALTER TABLE `transaction` ADD FOREIGN KEY (`fk_receiver_client`) REFERENCES `account_type` (`pk_account_type`);
+ALTER TABLE 'client_account_type' ADD FOREIGN KEY ('fk_client') REFERENCES 'client'('pk_client');
+ALTER TABLE 'client_account_type' ADD FOREIGN KEY ('fk_account_type') REFERENCES 'account_type'('pk_account_type');
 
-ALTER TABLE `agency_advisor` ADD FOREIGN KEY (`fk_agency`)  REFERENCES `agency` (`pk_agency`);
+ALTER TABLE 'transaction' ADD FOREIGN KEY ('fk_sender_account_type') REFERENCES 'account_type' ('pk_account_type');
+ALTER TABLE 'transaction' ADD FOREIGN KEY ('fk_receiver_account_type') REFERENCES 'account_type' ('pk_account_type');
+ALTER TABLE 'transaction' ADD FOREIGN KEY ('fk_sender_client') REFERENCES 'client' ('pk_client');
+ALTER TABLE 'transaction' ADD FOREIGN KEY ('fk_receiver_client') REFERENCES 'client' ('pk_client');
 
-ALTER TABLE `agency_advisor` ADD FOREIGN KEY (`fk_advisor`)  REFERENCES `advisor` (`pk_advisor`);
+ALTER TABLE 'agency_advisor' ADD FOREIGN KEY ('fk_agency')  REFERENCES 'agency' ('pk_agency');
 
-ALTER TABLE `agency` ADD FOREIGN KEY (`fw_address`)  REFERENCES `address` (`pk_address`);
+ALTER TABLE 'agency_advisor' ADD FOREIGN KEY ('fk_advisor')  REFERENCES 'advisor' ('pk_advisor');
+
+ALTER TABLE 'agency' ADD FOREIGN KEY ('fw_address')  REFERENCES 'address' ('pk_address');
--- main.py
+++ main.py
@@ -15,6 +15,46 @@ fake = Faker('fr_FR')
 print(mydb)
 mycursor = mydb.cursor()
 
+country = ['France', 'Canada', 'Suisse', 'Angleterre']
+
+account_type = [{
+  'name': 'Livret A',
+  'interest_rate': 3,
+}, {
+  'name': 'LDDS',
+  'interest_rate': 3,
+}, {
+  'name': 'LEP',
+  'interest_rate': 6.1,
+}, {
+  'name': 'Livret Jeune',
+  'interest_rate': 3,
+}, {
+  'name': 'CEL',
+  'interest_rate': 2,
+  'taxation': True
+}, {
+  'name': 'PEL',
+  'interest_rate': 2,
+  'taxation': True
+}, {
+  'name': 'PREP',
+  'interest_rate': None,
+  'taxation': True
+}, {
+  'name': 'CAT',
+  'interest_rate': 3,
+  'taxation': True
+}, {
+  'name': 'Compte Bancaire Individuel',
+  'interest_rate': None,
+  'is_external': True
+}, {
+  'name': 'Compte Bancaire Joint',
+  'interest_rate': None,
+  'is_external': True
+}]
+
 #######################################################################################
 # Utils
 #######################################################################################
@@ -26,24 +66,33 @@ def insert_many(table, fields, values, n_values):
   mydb.commit()
 
 
+def select(table, fields, conditions):
+  request = "SELECT {} FROM {} WHERE {}".format(fields, table, conditions)
+  mycursor.execute(request)
+  return mycursor.fetchall()
+
+
 #######################################################################################
 # Generators
 #######################################################################################
 
 
 # id, name vc(25)
-def insert_region(n):
+def insert_region(loop_number):
   values = []
-  for _ in range(n):
-    values.append((fake.unique.region(), ))
-  insert_many("region", "name", values, "%s")
+  for _ in range(loop_number):
+    values.append((
+      fake.unique.region(),
+      country[random.randint(0, 3)],
+    ))
+  insert_many("region", "name, country", values, "%s, %s")
 
 
 # id, code char(3), name varchar(25), id_region
-def insert_department(n):
+def insert_department(loop_number):
   id = 1
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
     dpt = fake.unique.department()
     values.append((
       dpt[0],
@@ -55,10 +104,10 @@ def insert_department(n):
 
 
 # id, name vc(50), postal_code vc(5), id_department
-def insert_city(n):
+def insert_city(loop_number):
   id = 1
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
     values.append((
       fake.unique.city(),
       fake.unique.postcode(),
@@ -69,35 +118,35 @@ def insert_city(n):
 
 
 # id, street_number smallint, street vc(250), address_complement vc(100), id_city
-def insert_address(n, max1):
+def insert_address(loop_number, max_fk_city):
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
     values.append((
       fake.building_number(),
       fake.street_name(),
       fake.street_prefix(),
-      random.randint(1, max1),
+      random.randint(1, max_fk_city),
     ))
   insert_many("address", "street_number, street, address_complement, fk_city",
               values, "%s, %s, %s, %s")
 
 
 # id, name vc(500), phone_number vc(13), id_address
-def insert_agency(n, max1):
+def insert_agency(loop_number, max_fw_address):
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
     values.append((
       fake.company(),
       fake.msisdn(),
-      random.randint(1, max1),
+      random.randint(1, max_fw_address),
     ))
   insert_many("agency", "name, phone_number, fw_address", values, "%s, %s, %s")
 
 
 # id, first_name, last_name, id_address
-def insert_advisor(n, max1):
+def insert_advisor(loop_number):
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
     values.append((
       fake.first_name(),
       fake.last_name(),
@@ -107,88 +156,144 @@ def insert_advisor(n, max1):
 
 
 # id_agency, id_advisor
-def insert_agency_advisor(n, max1, max2):
+def insert_agency_advisor(loop_number, max_fk_agency, max_fk_advisor):
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
     values.append((
-      random.randint(1, max1),
-      random.randint(1, max2),
+      random.randint(1, max_fk_agency),
+      random.randint(1, max_fk_advisor),
     ))
   insert_many("agency_advisor", "fk_agency, fk_advisor", values, "%s, %s")
 
 
-def insert_account_type(n):
+def insert_account_type():
   values = []
-  for _ in range(n):
-    start_date = fake.date_between()
-    end_date = datetime.date(2029, 1, 1)
+  for i in range(len(account_type)):
     values.append((
-      round(random.uniform(0.00, 9.99), 2),
-      start_date,
-      fake.date_between(start_date, end_date),
+      account_type[i]['name'],
+      account_type[i]['interest_rate'],
       random.randint(1, 3),
+      account_type[i]['taxation'] if 'taxation' in account_type[i] else False,
+      account_type[i]['is_external']
+      if 'is_external' in account_type[i] else False,
     ))
-  insert_many("account_type", "interest_rate, opening, ending, risk", values,
-              "%s, %s, %s, %s")
+  insert_many("account_type",
+              "name, interest_rate, risk, taxation, is_external", values,
+              "%s, %s, %s, %s, %s")
 
 
 # id, first_name, last_name, address, id_agency
-def insert_client(n, max):
+def insert_client(loop_number, max_fk_advisor, max_fk_address):
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
     values.append((
       fake.first_name(),
       fake.last_name(),
       random.randint(1, 15000),
-      random.randint(1, max),
+      random.randint(1, max_fk_advisor),
+      random.randint(1, max_fk_address),
     ))
-  insert_many("client", "first_name, last_name, salary_per_month, fk_advisor",
-              values, "%s, %s, %s, %s")
+  insert_many(
+    "client",
+    "first_name, last_name, salary_per_month, fk_advisor, fk_address", values,
+    "%s, %s, %s, %s, %s")
 
 
-def insert_client_account_type(n, max1, max2):
+def insert_client_account_type(loop_number, max_fk_client,
+                               max_fk_account_type):
   values = []
-  for _ in range(n):
+  for _ in range(loop_number):
+    start_date = fake.date_between()
+    end_date = datetime.date(2029, 1, 1)
     values.append((
-      random.randint(1, max1),
-      random.randint(1, max2),
+      random.randint(1, max_fk_client),
+      random.randint(1, max_fk_account_type),
+      start_date,
+      fake.date_between(start_date, end_date),
     ))
-  insert_many("client_account_type", "fk_client, fk_account_type", values,
-              "%s, %s")
+  insert_many("client_account_type",
+              "fk_client, fk_account_type, opening, ending", values,
+              "%s, %s, %s, %s")
 
 
 # id, amount, label, id_emitter, id_reciever
-def insert_transaction(n, max):
+def insert_transaction_of_current_vie(loop_number, max_fk_client):
   values = []
-  for _ in range(n):
+  account_type = select('account_type', 'pk_account_type', "is_external=1")
+  for _ in range(loop_number):
     start_date = fake.date_between()
     end_date = datetime.date(2029, 1, 1)
     values.append((
       random.randint(-10000, 10000),
-      random.randint(1, max),
-      random.randint(1, max),
       fake.date_between(start_date, end_date),
+      account_type[random.randint(0,
+                                  len(account_type) - 1)][0],
+      account_type[random.randint(0,
+                                  len(account_type) - 1)][0],
+      random.randint(1, max_fk_client),
+      random.randint(1, max_fk_client),
     ))
 
   # Allows to insert 100000 per 100000
   for element in [values[x:x + 100000] for x in range(0, len(values), 100000)]:
-    insert_many("transaction",
-                "amount, fk_sender_client, fk_receiver_client, emission",
-                element, "%s, %s, %s, %s")
+    insert_many(
+      "transaction",
+      "amount, emission, fk_sender_account_type, fk_receiver_account_type, fk_sender_client, fk_receiver_client",
+      element, "%s, %s, %s, %s, %s, %s")
+
+
+def insert_internal_transaction(loop_number, max_fk_client):
+  values = []
+  for _ in range(loop_number):
+    start_date = fake.date_between()
+    end_date = datetime.date(2029, 1, 1)
+
+    client_choose = random.randint(1, max_fk_client)
+
+    client_account_type = select(table='client_account_type',
+                                 fields='fk_account_type',
+                                 conditions='fk_client=' + str(client_choose))
+
+    size = len(client_account_type) - 1
+    if size > 0:
+      values.append((
+        random.randint(-10000, 10000),
+        fake.date_between(start_date, end_date),
+        client_account_type[random.randint(0,
+                                           len(client_account_type) - 1)][0],
+        client_account_type[random.randint(0,
+                                           len(client_account_type) - 1)][0],
+        client_choose,
+        client_choose,
+      ))
+    elif size <= 0:
+      continue
+
+  # Allows to insert 100000 per 100000
+  for element in [values[x:x + 100000] for x in range(0, len(values), 100000)]:
+    insert_many(
+      "transaction",
+      "amount, emission, fk_sender_account_type, fk_receiver_account_type, fk_sender_client, fk_receiver_client",
+      element, "%s, %s, %s, %s, %s, %s")
 
 
 #######################################################################################
 # Runners
 #######################################################################################
 
-insert_region(10)
-insert_department(10)
-insert_city(10)
-insert_address(1_000, 10)
-insert_agency(500, 1_000)
-insert_advisor(1_000, 1_000)
-insert_agency_advisor(250, 500, 1_000)
-insert_account_type(500)
-insert_client(10_000, 1_000)
-insert_client_account_type(10_000, 10_000, 500)
-insert_transaction(1_000_000, 10_000)
+insert_region(loop_number=10)
+insert_department(loop_number=10)
+insert_city(loop_number=10)
+insert_address(loop_number=1_000, max_fk_city=10)
+insert_agency(loop_number=500, max_fw_address=1_000)
+insert_advisor(loop_number=1_000)
+insert_agency_advisor(loop_number=250, max_fk_agency=500, max_fk_advisor=1_000)
+
+insert_account_type()
+insert_client(loop_number=10_000, max_fk_advisor=1_000, max_fk_address=1_000)
+insert_client_account_type(loop_number=20_000, max_fk_client=10_000, max_fk_account_type=len(account_type))
+
+insert_transaction_of_current_vie(loop_number=500_000, max_fk_client=10_000)
+insert_internal_transaction(loop_number=500_000, max_fk_client=10_000)
+
+print('OK')
